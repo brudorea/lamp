05506e4e89d525ff2fa341390cb2e00722ceb2c3
Bruno Henrique <brunodoreadelima@outlook.com>
2024-11-07 20:41:54 -0300

    quebrando lampada

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Simples simulador de uma lâmpada
+ * @author Bruno Henrique
+ */
+
+function quebrar() {
+    document.getElementById(`lamp`).src="img/broken.jpg"
+    // Reproduzindo um arquivo de água no JS
+    // Passo 1: copiar o arquivo de áudio para o projeto
+    // Passo 2: usar a classe áudio (biblioteca interna do JS)
+    let som = new Audio()
+    som.src ="sound/glassbreaking.wav"
+    som.play()
+}
